eed92fcdc16c039c2fa065069627362ff36012fc
MOHDSHABANKHAN <shabhankhan123456@gmail.com>
2023-09-11 19:01:59 +0530

    Our_Courses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 // import MasaiInNews from './Component/masaiInNews';
 import CiriculumAndProdogy from './Component/CiriculumAndProdogy';
+import Our_courses from './Component/Our_courses';
 function App() {
   return (
     <div className="App">
-      <CiriculumAndProdogy/>
+      {/* <CiriculumAndProdogy/> */}
+      <Our_courses/>
     {/* <MasaiInNews/> */}
     </div>
   );
