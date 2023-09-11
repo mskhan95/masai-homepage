cab397c39d85d366bfbec97a8b851faff5e46360
yash0922 <yashkumarpal987@gmail.com>
2023-09-11 16:24:14 +0530

    oneComponentIsadded

--- src/Component/CiriculumAndProdogy.jsx
+++ src/Component/CiriculumAndProdogy.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "../Style/CiriculumAndProdogy.css";
+import courseImgage1 from "../Image/course-image1.webp"
+import courseImgage2 from "../Image/course-image2.webp"
+import courseImgage3 from "../Image/course-image3.webp"
+import courseImgage4 from "../Image/course-image4.webp"
+import courseImgage5 from "../Image/course-image5.webp"
 function CiriculumAndProdogy(props) {
   return (
     <div>
@@ -24,7 +29,13 @@ function CiriculumAndProdogy(props) {
         </span>{" "}
       </h2>
       <div id="allScrolldivs">
-        <div id="courseText0"></div>
+        <div id="courseText0">
+          <h4>100% <span>live</span> distance learning</h4>
+          <p >
+            India’s only live tech-learning course with Tier 1 instructors. Get
+            real-time feedback, interactive sessions & a personalised learning
+            experience.
+          </p></div>
 
         <div id="courseText1"></div>
 
@@ -35,7 +46,11 @@ function CiriculumAndProdogy(props) {
         <div id="courseText4"></div>
 
         <div id="courseImage">
-            <img src="" alt="course" />
+            <img src={courseImgage1} alt="course" />
+            <img src={courseImgage2} alt="course" />
+            <img src={courseImgage3} alt="course" />
+            <img src={courseImgage4} alt="course" />
+            <img src={courseImgage5} alt="course" />
         </div>
       </div>
     </div>
