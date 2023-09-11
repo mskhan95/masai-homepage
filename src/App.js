1de46aeb928e359321d886be4021e45a242ed1fe
Abhishek kumar <abhishekkumarverma7256@gmail.com>
2023-09-11 20:00:44 +0530

    [Details]: Navbar and Footer added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import './App.css';
 
 // import MasaiInNews from './Component/masaiInNews';
 // import CiriculumAndProdogy from './Component/CiriculumAndProdogy';
-// import Navbar from './Component/Navbar';
+ import Navbar from './Component/Navbar';
  import Desktop from './Component/Desktop'
 import Our_courses from './Component/Our_courses';
-  
+import Footer from './Component/Footer';
 import CarrearPossibility from './Component/CarrearPossibility';
 function App() {
   return (
     <div className="App">
+
+     <Navbar/>
       {/* <CiriculumAndProdogy/> */}
     {/* <MasaiInNews/> */}
     <CarrearPossibility/>
-    {/* <Navbar/> */}
+    
     {/* <Desktop/> */}
   <Our_courses/>
+
+  <Footer/>
+   </div>
   );
 }
 
