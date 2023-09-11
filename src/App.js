9d9f7eea560bfeeeb35007167debbe5471ceb8a0
MOHDSHABANKHAN <shabhankhan123456@gmail.com>
2023-09-11 19:09:36 +0530

    Our_Courses

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-// import MasaiInNews from './Component/masaiInNews';
-// import CiriculumAndProdogy from './Component/CiriculumAndProdogy';
+import MasaiInNews from './Component/masaiInNews';
+import CiriculumAndProdogy from './Component/CiriculumAndProdogy';
 // import Navbar from './Component/Navbar';
+
 import CarrearPossibility from './Component/CarrearPossibility';
+import Our_courses from './Component/Our_courses';
 function App() {
   return (
     <div className="App">
-      {/* <CiriculumAndProdogy/> */}
-    {/* <MasaiInNews/> */}
-<CarrearPossibility/>
+    <Our_courses/> 
+    <CiriculumAndProdogy/>
+    <MasaiInNews/>
+    <CarrearPossibility/>
+    
     {/* <Navbar/> */}
+  
     </div>
   );
 }
